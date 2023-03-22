31c34d356a3d8a8b64050aad22a8d30c4945c508
Igor Telmenko <igor-telmenko@yandex.ru>
2023-03-22 20:52:16 +0300

    Init commit

---
+++ precache-manifest.c7ebb3ebafed45fdc4378f7cfffd6b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b47a61498ae13e6d3e458d4882daeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ffbc48fe7ff0e10113f",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "3a37c87d825212c9acf3",
+    "url": "/static/js/2.00fb7832.chunk.js"
+  },
+  {
+    "revision": "3ffbc48fe7ff0e10113f",
+    "url": "/static/js/main.18699535.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
